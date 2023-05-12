34c485e14305ab1cab2890200caa862392052e4e
Akshat Jaimini <destrex271@gmail.com>
2023-05-12 15:20:20 +0530

    testing build

---
+++ src/components/ProjectsPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="prj-body">
+    <div>
+    </div>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Octokit } from "octokit";
+
+export default {
+  name: 'ProjectsPage',
+  setup() {
+    // Test Comment for push
+    const octokit = new Octokit({
+    })
+  }
+}
+</script>
+
+<style>
+.prj-body {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
